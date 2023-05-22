ffb841bba4d9024b58bf0c61dcd46de6a14aa4a4
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2023-05-22 23:14:51 +0200

    [FEATURE] General refactoring.

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,15 @@ use crate::word_collection_memory::dependency::Dependency;
 use word_collection_memory::WordCollectionMemory;
 
 fn main() {
-    let mut collection = WordCollectionMemory::new();
+    let mut collection = get_collection();
     if let Err(e) = collection.on_init() {
         eprintln!("{}", e);
     }
     println!("{:?}", collection.find(&"Almendralejo".to_string()));
     println!("---------------------------------------------");
     println!("{:?}", collection.find_includes(&"Ab".to_string()));
+}
+
+fn get_collection() -> impl WordCollection + Dependency {
+    return WordCollectionMemory::new();
 }
